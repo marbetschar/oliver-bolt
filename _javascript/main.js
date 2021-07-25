cc38aa505ecf417c8beadefbc2774327e64f976a
Marco Betschart <email@marco.betschart.name>
2021-07-25 15:40:53 +0200

    Index + About

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,26 @@
+const init = {
+  navbarBurgers: () => {
+      const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('a.navbar-burger'), 0);
+      if (navbarBurgers.length > 0) {
+        navbarBurgers.forEach(navbarBurger => {
+          navbarBurger.addEventListener('click', e => {
+            let eventTarget = e.target;
+            if (eventTarget.tagName.toLowerCase() != 'a') {
+              eventTarget = e.target.parentElement;
+            }
+            const navbarTarget = document.getElementById(eventTarget.dataset.target);
+
+            if (navbarTarget) {
+              navbarBurger.classList.toggle('is-active');
+              navbarTarget.classList.toggle('is-hidden-touch');
+              navbarTarget.classList.toggle('is-block');
+            }
+          });
+        });
+      }
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+  init.navbarBurgers();
 });
